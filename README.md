2963ba168c062b3a990c62d14d33d079329b8b93
aman godara <amangodara005@gmail.com>
2025-12-10 00:01:16 +0530

    day 1 basic setup done

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Airbnb
 A   mini clone of Airbnb website as  a  project 
+
+# progress report
+DAY 1 -  basic setup, build database connection, first git commit 
+         faced many errors while working with repo, rookie mistakes! , need to improve!
--- app.js
+++ app.js
@@ -1,3 +1,26 @@
-const express = requre("express");
+const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const port = 3000;
+app.listen(port, ()=>{
+    console.log("starting at:",port);
+});
+
+const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderlust';
+
+//calling main function
+main().then(()=>{
+    console.log("connection successful");
+}).catch((err)=>{
+    console.log(err);
+});
+
+//setting up connection
+async function main(){
+    await mongoose.connect(MONGO_URL);
+};
+
+//routes
+app.get("/",(req,res)=>{
+    res.send("root");
+});
